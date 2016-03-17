8a7318308aef8c1f51d20f266795d3c4ec372c38
Jakob Mass <jaks@ut.ee>
2016-03-17 18:00:38 +0200

    Initial commit

---
+++ src/main/java/read_file_workflow/ReadFileWorkflowApp.java
@@ -0,0 +1,9 @@
+package read_file_workflow;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Read File Pi Workflow App")
+public class ReadFileWorkflowApp extends ServletProcessApplication{
+
+}
